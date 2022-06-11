8741f6fd034d9eb6c64d5b54e4cd42131aca49ec
Deividson Damasio <deividsondamasio@yahoo.com.br>
2022-06-11 06:06:03 -0300

    Example with remove :type

--- resources/views/components/documents/index/buttons.blade.php
+++ resources/views/components/documents/index/buttons.blade.php
@@ -2,14 +2,14 @@
     @can($permissionCreate)
         @if (! $hideCreate)
             <x-link href="{{ route($createRoute) }}" kind="primary">
-                {{ trans('invoices.new_invoice', ['type' => trans_choice($textPage, 1)]) }}
+                {{ trans('invoices.new_invoice') }}
             </x-link>
         @endif
     @endcan
 @else
     @if (! $hideCreate)
         <x-link href="{{ route($createRoute) }}" kind="primary">
-            {{ trans('invoices.new_invoice', ['type' => trans_choice($textPage, 1)]) }}
+            {{ trans('invoices.new_invoice') }}
         </x-link>
     @endif
 @endif
